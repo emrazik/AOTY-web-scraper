ae31c89e672c46aef7aefd62c1880ad203190659
emrazik <mrazik.evan@gmail.com>
2022-10-26 22:54:21 -0700

    improving visual output (colors and formatting)

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from cgitb import text
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 import argparse
 import sys
 
 FUNCTIONS = {'-AS'}
+
+class textcolors:
+    GREEN = '\u001b[32m'
+    YELLOW = '\u001b[33m'
+    RED = '\u001b[31m'
+    END = '\u001b[0m'
 
 def album_search(args):
     query_list = args.artist.split() + args.album.split()
@@ -20,11 +27,24 @@ def album_search(args):
         page.click('.albumBlock')
         html = page.inner_html('body')
         html_parsed = BeautifulSoup(html, 'html.parser')
-        print(html_parsed.find('div', {'class':'albumTitle'}).text)
-        print(html_parsed.find('div', {'class':'artist'}).text)
-        print(html_parsed.find('a', {'href':'#critics'}).text)
-        print(html_parsed.find('a', {'href':'#users'}).text)
+        print('Artist: ' + html_parsed.find('div', {'class':'artist'}).text)
+        print('Album: ' + html_parsed.find('div', {'class':'albumTitle'}).text + '\n')
+        
+        critic_score = int(html_parsed.find('a', {'href':'#critics'}).text)
+        user_score = int(html_parsed.find('a', {'href':'#users'}).text)
+        color_code = handle_text_color(critic_score)
+        print('Critics: ' + color_code + str(critic_score) + textcolors.END)
+        color_code = handle_text_color(user_score)
+        print('Users: ' + color_code + str(user_score) + textcolors.END)
 
+def handle_text_color(score):
+    if score > 70:
+        return textcolors.GREEN
+    elif score > 50:
+        return textcolors.YELLOW
+    else:
+        return textcolors.RED
+    
 def console_output():
     pass
 
@@ -34,10 +54,11 @@ def console_output_verbose():
 def main():
     parser = argparse.ArgumentParser(description='Search for information on albumoftheyear.com')
     parser.add_argument('-AS', action='store_true', help='Search for an album')
-    parser.add_argument('-al', '--artist', type=str, required=any(func in sys.argv for func in FUNCTIONS), help='Name of a musical artist', metavar='')
-    parser.add_argument('-ar', '--album', type=str, required=any(func in sys.argv for func in FUNCTIONS), help='Name of an album', metavar='')
+    parser.add_argument('-ar', '--artist', type=str, required=any(func in sys.argv for func in FUNCTIONS), help='Name of a musical artist', metavar='')
+    parser.add_argument('-al', '--album', type=str, required=any(func in sys.argv for func in FUNCTIONS), help='Name of an album', metavar='')
     args = parser.parse_args()
 
+    print(args)
     if args.AS:
         album_search(args)
  
