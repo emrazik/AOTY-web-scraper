f8f521e3e5a43da09af3f063c5530b584b256a6b
emrazik <mrazik.evan@gmail.com>
2022-10-26 22:32:04 -0700

    basic CLI argument parsing

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
+import argparse
+import sys
 
-def album_search(artist_name, album_name):
+FUNCTIONS = {'-AS'}
+
+def album_search(args):
+    query_list = args.artist.split() + args.album.split()
+
+    format_query = 'https://www.albumoftheyear.org/search/?q=%'
+    for item in query_list:
+        format_query += item + '+'   
+    format_query = format_query[:-1]
+
     with sync_playwright() as p:
-        format_query = f'https://www.albumoftheyear.org/search/?q={artist_name}+{album_name}'
         browser = p.firefox.launch()
         page = browser.new_page()
         page.goto(format_query)
@@ -22,9 +32,14 @@ def console_output_verbose():
     pass
 
 def main():
-    artist_name = input("What artist? ")
-    album_name = input("What album? ")
-    album_search(artist_name, album_name)
+    parser = argparse.ArgumentParser(description='Search for information on albumoftheyear.com')
+    parser.add_argument('-AS', action='store_true', help='Search for an album')
+    parser.add_argument('-al', '--artist', type=str, required=any(func in sys.argv for func in FUNCTIONS), help='Name of a musical artist', metavar='')
+    parser.add_argument('-ar', '--album', type=str, required=any(func in sys.argv for func in FUNCTIONS), help='Name of an album', metavar='')
+    args = parser.parse_args()
+
+    if args.AS:
+        album_search(args)
  
 if __name__ == "__main__":
     main()
